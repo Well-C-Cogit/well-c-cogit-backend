dc11fe8ecb9cd560aa8ea6ad1d676efeb69ac70f
kangsan <devstar1223@gmail.com>
2024-09-05 17:28:38 +0900

    Initial commit

---
+++ src/main/java/com/teamwccg/well_c_cogit_backend/WellCCogitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.teamwccg.well_c_cogit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellCCogitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellCCogitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamwccg/well_c_cogit_backend/WellCCogitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamwccg.well_c_cogit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellCCogitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
